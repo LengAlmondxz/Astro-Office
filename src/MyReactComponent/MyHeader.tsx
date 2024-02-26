a9d8e5c13482f506767ea58524a27a63f94dfc0a
Leng Almodiel <leng.almodiel.intern@tier.one>
2024-02-26 15:13:29 +0800

    fix this file

--- src/MyReactComponent/MyHeader.tsx
+++ src/MyReactComponent/MyHeader.tsx
@@ -1,13 +1,15 @@
-export default function MyHeader(props) {
+export const MyHeader = (props) => {
   return (
     <div className="border border-black">
-      <header class="text-xl text-pink-400 font-semibold">{props.title}</header>
-      <header class="text-xl text-violet-400 font-semibold">
+      <header className="text-xl text-pink-400 font-semibold">
+        {props.title}
+      </header>
+      <header className="text-xl text-violet-400 font-semibold">
         {props.subTitle}
       </header>
-      <footer class="text-xl text-blue-400 font-semibold">
+      <footer className="text-xl text-blue-400 font-semibold">
         {props.socialLinks}
       </footer>
     </div>
   );
-}
+};
