84b20af880a4a2137996860a2a8d39fc9d160d05
Leng Almodiel <leng.almodiel.intern@tier.one>
2024-02-26 15:08:36 +0800

    changes to deploy

--- src/MyReactComponent/MyReactComponent.tsx
+++ src/MyReactComponent/MyReactComponent.tsx
@@ -21,27 +21,27 @@ export default function SetCount() {
   return (
     <>
       <div>
-        <div class="flex gap-2 mt-5 ml-2 justify-center">
+        <div className="flex gap-2 mt-5 ml-2 justify-center">
           <button
             onClick={incrementCount}
-            class="px-5 py-2 border rounded-md font-semibold bg-red-200 shadow-lg "
+            className="px-5 py-2 border rounded-md font-semibold bg-red-200 shadow-lg "
           >
             +1
           </button>
           <button
             onClick={decrementCount}
-            class="px-5 py-2 border rounded-md font-semibold bg-yellow-200 shadow-lg "
+            className="px-5 py-2 border rounded-md font-semibold bg-yellow-200 shadow-lg "
           >
             -1
           </button>
           <button
             onClick={reset}
-            class="px-5 py-2 border rounded-md font-semibold bg-green-200 shadow-lg "
+            className="px-5 py-2 border rounded-md font-semibold bg-green-200 shadow-lg "
           >
             Reset
           </button>
         </div>
-        <p class="text-6xl text-center mt-8">{count}</p>
+        <p className="text-6xl text-center mt-8">{count}</p>
       </div>
     </>
   );
